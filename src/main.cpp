28fc0269f3b2d5bcdde71a3f2630f9acdc72bc33
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-01-11 12:54:23 +0100

    Test fix daemon mode Windows 6

--- src/main.cpp
+++ src/main.cpp
@@ -1097,8 +1097,8 @@ static int run_daemon_mode(ProcessParams &params)
             break; // EOF or error
         }
 
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
-        line = conv.to_bytes(wline);
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv1;
+        line = conv1.to_bytes(wline);
 #else
         if (!std::getline(std::cin, line))
         {
@@ -1157,11 +1157,11 @@ static int run_daemon_mode(ProcessParams &params)
 
         std::vector<std::wstring> wargs;
         std::vector<wchar_t*> wargv;
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv2;
 
         for (int i = 0; i < argc; i++)
         {
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
-            std::wstring wstr = conv.from_bytes(argv[i]);
+            std::wstring wstr = conv2.from_bytes(argv[i]);
             wargs.push_back(wstr);
             wargv.push_back(wargs.back().data());
         }
